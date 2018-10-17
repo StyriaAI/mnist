0ac05be85834ed022824fcf75d59ee8baf259c3b
Vjekoslav Giacometti <vjekoslav.giacometti@styria.hr>
2018-10-17 09:12:45 +0200

    Initial commit.

---
+++ api.py
@@ -0,0 +1,28 @@
+import logging
+from io import BytesIO
+
+from flask import Blueprint, request, current_app
+
+import config
+from helpers import ApiException, generate_ok_response, preprocess_jpeg_image
+
+bp = Blueprint('digit', __name__)
+
+
+@bp.route("/", methods=["GET"])
+def status():
+    return generate_ok_response("API running")
+
+
+@bp.route("/classify", methods=["POST"])
+def classify():
+    raw_img = BytesIO(request.get_data())
+
+    try:
+        img = preprocess_jpeg_image(raw_img, config.target_image_shape)
+    except Exception as e:
+        logging.exception(e)
+        raise ApiException("Invalid image")
+
+    digit = current_app.model_client.classify(img)
+    return generate_ok_response({"digit": digit})
---
+++ config.py
@@ -0,0 +1,11 @@
+# model server uri in format host:port
+model_server_uri = "localhost:9000"
+
+# name of the model
+model_name = "mnist"
+
+# shape of the image expected by the model
+target_image_shape = (28, 28)
+
+# port to use in development
+dev_port = 5000
